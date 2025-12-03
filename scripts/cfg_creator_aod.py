8862fedeed06378e551a179a05d25eb061d8589a
fernance <fernance@cern.ch>
2025-12-03 18:29:05 +0100

    Add files for 2022 production

--- scripts/cfg_creator_aod.py
+++ scripts/cfg_creator_aod.py
@@ -1,51 +1,57 @@
 import os
 
-p = "/home/hep/jleonhol/samplegeneration/gen/nicepythia/CMSSW_13_2_0/src/Configuration/GenProduction/data/"
-files = os.listdir(p)
-
 crab = """
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = '{name}'
-config.General.workArea = '{name}'
+config.General.requestName = '2022/{name}'
+config.General.workArea = '2022/{name}'
 config.General.transferOutputs = True
-config.General.transferLogs = True
+config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'aod_cfg.py'
-config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'aod_cfg_skim.py'
+config.JobType.maxMemoryMB = 3000
 # config.JobType.numCores = 8
 
 config.Data.inputDataset = '{dataset}'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
-NJOBS = 100
-config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.totalUnits = -1
 config.Data.inputDBS = 'phys03'
 
-config.Data.outLFNDirBase = '/store/user/jleonhol/samples/AODSIM/'
+config.Data.outLFNDirBase = '/store/user/mmasciov/dqcd-samples/AODSIM/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'AODSIM_2022'
+config.Data.outputDatasetTag = 'AODSIM_2022-final'
 
-config.Site.storageSite = 'T2_UK_London_IC'
+config.Site.whitelist = ['T2_US_Wisconsin','T2_US_Florida','T2_US_Nebraska','T2_US_Caltech','T2_US_Purdue', 'T2_US_UCSD']
+config.Site.storageSite = 'T2_US_UCSD'
 """
 
+
 datasets = {
-    "scenarioA_mpi_4_mA_1p33_ctau_10": "/scenarioA_mpi_4_mA_1p33_ctau_10/jleonhol-scenarioA_mpi_4_mA_1p33_ctau_10_2022-ecf4a583228f65e16019e00d96f41f77/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_0p1": "/scenarioA_mpi_4_mA_1p33_ctau_0p1/jleonhol-scenarioA_mpi_4_mA_1p33_ctau_0p1_2022-0df1009824dae361041086fdd8b62230/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_100": "/scenarioA_mpi_4_mA_1p33_ctau_100/jleonhol-scenarioA_mpi_4_mA_1p33_ctau_100_2022-083453e5d820d4dc4b07e180627c561e/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_1p0": "/scenarioA_mpi_4_mA_1p33_ctau_1p0/jleonhol-scenarioA_mpi_4_mA_1p33_ctau_1p0_2022-53a0d741c19eae261a06f270970f54e0/USER",
+    "scenarioA_mpi_12_mA_1p20_ctau_1000": "/scenarioA_mpi_12_mA_1p20_ctau_1000/mmasciov-scenarioA_mpi_12_mA_1p20_ctau_1000_2022-3c3edc919e7c4668e1ab9290e5275a95/USER",
+    "scenarioA_mpi_2_mA_0p67_ctau_1000": "/scenarioA_mpi_2_mA_0p67_ctau_1000/mmasciov-scenarioA_mpi_2_mA_0p67_ctau_1000_2022-9117bc358dec59e63135080854cf2478/USER",
+    "scenarioA_mpi_4_mA_1p33_ctau_1000": "/scenarioA_mpi_4_mA_1p33_ctau_1000/mmasciov-scenarioA_mpi_4_mA_1p33_ctau_1000_2022-55899c43e6b6776961f2faaae3a6c05c/USER",
+    "scenarioA_mpi_5_mA_1p67_ctau_1000": "/scenarioA_mpi_5_mA_1p67_ctau_1000/mmasciov-scenarioA_mpi_5_mA_1p67_ctau_1000_2022-0380d94853290679e84b1e5add533534/USER",
+    "scenarioA_mpi_6_mA_2_ctau_1000": "/scenarioA_mpi_6_mA_2_ctau_1000/mmasciov-scenarioA_mpi_6_mA_2_ctau_1000_2022-36156b5ae2ad240e0af78eb06727803a/USER",
+    "scenarioA_mpi_7p50_mA_2p50_ctau_1000": "/scenarioA_mpi_7p50_mA_2p50_ctau_1000/mmasciov-scenarioA_mpi_7p50_mA_2p50_ctau_1000_2022-825825fb24ddc4f3a66c5f8c2d822709/USER",
+    "scenarioB1_mpi_12_mA_1p20_ctau_1000": "/scenarioB1_mpi_12_mA_1p20_ctau_1000/mmasciov-scenarioB1_mpi_12_mA_1p20_ctau_1000_2022-866aa73572dc953161cb1166731afe1b/USER",
+    "scenarioB1_mpi_2_mA_0p67_ctau_1000": "/scenarioB1_mpi_2_mA_0p67_ctau_1000/mmasciov-scenarioB1_mpi_2_mA_0p67_ctau_1000_2022-54a1c975abb4c7b82cd4e24ed2fc5a56/USER",
+    "scenarioB1_mpi_4_mA_1p33_ctau_1000": "/scenarioB1_mpi_4_mA_1p33_ctau_1000/mmasciov-scenarioB1_mpi_4_mA_1p33_ctau_1000_2022-1ed602d2c83d952e0f7187f9e8584944/USER",
+    "scenarioB1_mpi_5_mA_1p67_ctau_1000": "/scenarioB1_mpi_5_mA_1p67_ctau_1000/mmasciov-scenarioB1_mpi_5_mA_1p67_ctau_1000_2022-7479e51b961544d6c66b008a70bae9df/USER",
+    "scenarioB1_mpi_6_mA_2_ctau_1000": "/scenarioB1_mpi_6_mA_2_ctau_1000/mmasciov-scenarioB1_mpi_6_mA_2_ctau_1000_2022-d62efb42a097d0667004ec3b1320082c/USER",
+    "scenarioB1_mpi_7p50_mA_2p50_ctau_1000": "/scenarioB1_mpi_7p50_mA_2p50_ctau_1000/mmasciov-scenarioB1_mpi_7p50_mA_2p50_ctau_1000_2022-7d9c5fc6b6ccbb36bdb206a640eea33a/USER"
 }
 
 for name, dataset in datasets.items():
     # name = f.split(".")[0]
     # print(cmnd.format(name=name))
-    # print(name)
-    if os.path.exists(f"{name}/crab_{name}"):
+    print(name)
+    if os.path.exists(f"2022/{name}/crab_{name}"):
         continue
     #os.system(cmnd.format(name=name))
-    with open("crab_submit_%s.py" % name, "w+") as f:
+    with open("2022/crab_submit_%s.py" % name, "w+") as f:
         f.write(crab.format(name=name, dataset=dataset))
-    os.system("crab submit crab_submit_%s.py" % name)
+    #os.system("crab submit 2022/crab_submit_%s.py" % name)
 
--- scripts/cfg_creator_postEE_aod.py
+++ scripts/cfg_creator_postEE_aod.py
@@ -1,51 +1,56 @@
 import os
 
-p = "/vols/cms/jtafoyav/production/CMSSW_13_2_0/src/Configuration/GenProduction/data"
-files = os.listdir(p)
-
 crab = """
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = '{name}'
-config.General.workArea = '{name}'
+config.General.requestName = '2022postEE/{name}'
+config.General.workArea = '2022postEE/{name}'
 config.General.transferOutputs = True
-config.General.transferLogs = True
+config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'aod_postEE_cfg.py'
-config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'aod_postEE_skim_cfg.py'
+config.JobType.maxMemoryMB = 3000
 # config.JobType.numCores = 8
 
 config.Data.inputDataset = '{dataset}'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
-NJOBS = 100
-config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.totalUnits = -1
 config.Data.inputDBS = 'phys03'
 
-config.Data.outLFNDirBase = '/store/user/tafoyava/samples/AODSIM/'
+config.Data.outLFNDirBase = '/store/user/mmasciov/dqcd-samples/AODSIM/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'AODSIM_2022'
+config.Data.outputDatasetTag = 'AODSIM_2022postEE'
 
+config.Site.whitelist = ['T2_US_Wisconsin','T2_US_Florida','T2_US_Nebraska','T2_US_Caltech','T2_US_Purdue', 'T2_US_UCSD']
 config.Site.storageSite = 'T2_US_UCSD'
 """
 
 datasets = {
-    "scenarioA_mpi_4_mA_1p33_ctau_10": "/scenarioA_mpi_4_mA_1p33_ctau_10/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_10_2022-7f5f2da1509daceb668e678ad7f6c826/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_100": "/scenarioA_mpi_4_mA_1p33_ctau_100/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_100_2022-0d52a2f512a8dff64215063478ed58d4/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_0p1": "/scenarioA_mpi_4_mA_1p33_ctau_0p1/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_0p1_2022-a9026f8b948d21e523e910a9f4d11f1c/USER",
-    "scenarioA_mpi_4_mA_1p33_ctau_1p0": "/scenarioA_mpi_4_mA_1p33_ctau_1p0/tafoyava-scenarioA_mpi_4_mA_1p33_ctau_1p0_2022-9250a3d6ebbbc29553c17fb39e5f0303/USER",
+    "scenarioA_mpi_12_mA_1p20_ctau_1000": "/scenarioA_mpi_12_mA_1p20_ctau_1000/mmasciov-scenarioA_mpi_12_mA_1p20_ctau_1000_2022postEE-9fde3cc31e6376df0c95839d63d71c55/USER",
+    "scenarioA_mpi_2_mA_0p67_ctau_1000": "/scenarioA_mpi_2_mA_0p67_ctau_1000/mmasciov-scenarioA_mpi_2_mA_0p67_ctau_1000_2022postEE-22efb66cde078fe5ce014a25a9b6d9d0/USER",
+    "scenarioA_mpi_4_mA_1p33_ctau_1000": "/scenarioA_mpi_4_mA_1p33_ctau_1000/mmasciov-scenarioA_mpi_4_mA_1p33_ctau_1000_2022postEE-7357338f88afc1390512c31fca75bc77/USER",
+    "scenarioA_mpi_5_mA_1p67_ctau_1000": "/scenarioA_mpi_5_mA_1p67_ctau_1000/mmasciov-scenarioA_mpi_5_mA_1p67_ctau_1000_2022postEE-37b846698d3b06f3b9397469cb57bfd8/USER",
+    "scenarioA_mpi_6_mA_2_ctau_1000": "/scenarioA_mpi_6_mA_2_ctau_1000/mmasciov-scenarioA_mpi_6_mA_2_ctau_1000_2022postEE-68e2f3f786024f7f52b8d997842a93fb/USER",
+    "scenarioA_mpi_7p50_mA_2p50_ctau_1000": "/scenarioA_mpi_7p50_mA_2p50_ctau_1000/mmasciov-scenarioA_mpi_7p50_mA_2p50_ctau_1000_2022postEE-2361fa64a45e1e7dd25590f33ef552d9/USER",
+    "scenarioB1_mpi_12_mA_1p20_ctau_1000": "/scenarioB1_mpi_12_mA_1p20_ctau_1000/mmasciov-scenarioB1_mpi_12_mA_1p20_ctau_1000_2022postEE-4085e864ea2d360bc9b81bb00f71f653/USER",
+    "scenarioB1_mpi_2_mA_0p67_ctau_1000": "/scenarioB1_mpi_2_mA_0p67_ctau_1000/mmasciov-scenarioB1_mpi_2_mA_0p67_ctau_1000_2022postEE-aca13f004b6e680f1852c8c033bbdde3/USER",
+    "scenarioB1_mpi_4_mA_1p33_ctau_1000": "/scenarioB1_mpi_4_mA_1p33_ctau_1000/mmasciov-scenarioB1_mpi_4_mA_1p33_ctau_1000_2022postEE-59e616efd7dcc96705034f3951bca811/USER",
+    "scenarioB1_mpi_5_mA_1p67_ctau_1000": "/scenarioB1_mpi_5_mA_1p67_ctau_1000/mmasciov-scenarioB1_mpi_5_mA_1p67_ctau_1000_2022postEE-0cb45119dbde112629741e8d60bed98e/USER",
+    "scenarioB1_mpi_6_mA_2_ctau_1000": "/scenarioB1_mpi_6_mA_2_ctau_1000/mmasciov-scenarioB1_mpi_6_mA_2_ctau_1000_2022postEE-9e58c09853d0619413574819b9408fdf/USER",
+    "scenarioB1_mpi_7p50_mA_2p50_ctau_1000": "/scenarioB1_mpi_7p50_mA_2p50_ctau_1000/mmasciov-scenarioB1_mpi_7p50_mA_2p50_ctau_1000_2022postEE-4db0050299428e8fc96157ea92f1b4d9/USER"
 }
 
 for name, dataset in datasets.items():
     # name = f.split(".")[0]
     # print(cmnd.format(name=name))
     # print(name)
-    if os.path.exists(f"{name}/crab_{name}"):
+    if os.path.exists(f"2022postEE/{name}/crab_{name}"):
         continue
     #os.system(cmnd.format(name=name))
-    with open("crab_submit_%s.py" % name, "w+") as f:
+    with open("2022postEE/crab_submit_%s.py" % name, "w+") as f:
         f.write(crab.format(name=name, dataset=dataset))
-    os.system("crab submit crab_submit_%s.py" % name)
+    #os.system("crab submit 2022postEE/crab_submit_%s.py" % name)
 
